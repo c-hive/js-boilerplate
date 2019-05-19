a31584f71cc042f1ee0f9894ea0a8c7dcf378002
Csaba Apagyi <csaba.apagyi@gmail.com>
2019-05-19 14:40:03 +0200

    Move comma dangle rules to prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,12 +8,5 @@ module.exports = {
     "linebreak-style": ["error", (process.platform === "win32" ? "windows" : "unix")], // https://stackoverflow.com/q/39114446/2771889
     "import/prefer-default-export": 0, // https://stackoverflow.com/q/54245654/2771889
     "react/sort-comp": "off", // https://github.com/yannickcr/eslint-plugin-react/issues/1214
-    "comma-dangle": ["error", { // Makes diffs cleaner
-        "arrays": "always-multiline",
-        "objects": "always-multiline",
-        "imports": "always-multiline",
-        "exports": "always-multiline",
-        "functions": "ignore" // Not backward compatible, requires ECMAScript 2017+
-    }],
   },
 };
---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+// If possible, settings should be kept in 1 place (i.e. `eslintrc`)
+// But in some cases ESLing and Prettier confuse eachother
+module.exports = {
+  trailingComma: "es5", // Makes diffs nicer
+}
